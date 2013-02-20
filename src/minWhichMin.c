44829f800c6bedc7714352ba46fe0de1c1a81fce
Peter Langfelder <Peter.Langfelder@gmail.com>
2013-02-20 00:00:00 +0000

    version 1.4

---
+++ src/minWhichMin.c
@@ -0,0 +1,31 @@
+/*
+
+  Function returning the column-wise minimum and minimum index. For easier integration with R, the index
+will also be stored as a double. NA's are ignored.
+
+*/
+
+#include <R.h>
+#include <R_ext/BLAS.h>
+#include <R_ext/libextern.h>
+
+
+void minWhichMin(double * matrix, int * nRows, int * nColumns, double * min, double * whichMin)
+{
+  int nrows = *nRows, ncols = *nColumns;
+
+  for (int i=0; i<ncols; i++)
+  {
+    double * col = matrix + i*nrows;
+    double curmin = *col;
+    double curwhich = 0;
+    for (int j=1; j<nrows; j++)
+    {
+      col++;
+      if ( ISNAN(curmin) || (!ISNAN(*col) && (*col < curmin))) { curmin = *col; curwhich = (double) j; }
+    }
+    min[i] = curmin;
+    whichMin[i] = curwhich;
+  }
+}
+
